10bf68e784e2f55be96839cb1d0993667d3da0b2
larkvi <sean.winslow@uni-graz.at>
2023-06-19 14:35:39 +0200

    added examples. Script temporarily hard-coded for working demo

--- rti_loop.py
+++ rti_loop.py
@@ -8,7 +8,7 @@ import subprocess
 pixels1 = neopixel.NeoPixel(board.D18, 50, brightness=1) #there are only four acceptable pins for the data. I am using 18.
 
 base_path = '/home/CameraZero/Pictures/RTI'
-batchNumber = 1 #make this increment automatically based upon the current highest-numbered batch -- perhaps the user passes a name (presumably of the object), and then each subsequent call with that name increments?
+batchNumber = 2 #make this increment automatically based upon the current highest-numbered batch -- perhaps the user passes a name (presumably of the object), and then each subsequent call with that name increments?
 
 def clearPixels():
     pixels1.fill((0,0,0)) #is there a better way to turn the neopixels off?
@@ -23,14 +23,15 @@ def makeBatchFolder(base_path,batchNumber):
         print(error)
     return 
         
-def take_the_photo(batchNumber,lightNumber):
+def take_the_photo(target_folder,batchNumber,lightNumber):
     camera = gp.Camera()
     camera.init()
     text = camera.get_summary()
     file_path = camera.capture(gp.GP_CAPTURE_IMAGE)
     print('Camera filepath: {0}/{1}'.format(file_path.folder, file_path.name))
-    print("Directory %s created." % target_folder)
-    target = os.path.join(target_folder,str(batchNumber).zfill(3)+'_'+str(lightNumber).zfill(2)+'_'+file_path.name)
+#    print("Directory %s created." % target_folder)
+#    target = os.path.join(target_folder,str(batchNumber).zfill(3)+'_'+str(lightNumber).zfill(2)+'_'+file_path.name)
+    target = os.path.join('/home/CameraZero/Pictures/RTI/batch_002',str(batchNumber).zfill(3)+'_'+str(lightNumber).zfill(2)+'_'+file_path.name)
 #    target = os.path.join('/tmp','Batch'+str(batchNumber).zfill(3),str(batchNumber)+'_'+str(lightNumber)+file_path.name)
     print('Copying image to', target)
     camera_file = camera.file_get(
@@ -40,7 +41,7 @@ def take_the_photo(batchNumber,lightNumber):
 #    camera.exit() #apparently un-needed
     return
 
-def lightAndShoot(batchNumber,lightNumber): 
+def lightAndShoot(target_folder,batchNumber,lightNumber): 
     clearPixels()
     pixels1[lightNumber] = (255,255,255)
     take_the_photo(target_folder,batchNumber,lightNumber)
@@ -49,8 +50,8 @@ def lightAndShoot(batchNumber,lightNumber):
 
 def batchLightAndShoot(base_path,batchNumber): #figure out how to ensure the camera is awake // can gphoto2 wake the camera?
     target_folder = makeBatchFolder(base_path,batchNumber)
-    for lightNumber in range (0,39):
-        print('Batch: '+batchNumber+'. Image: '+str(lightNumber)+' .')
+    for lightNumber in range (37,49):
+        print('Batch: '+str(batchNumber)+'. Image: '+str(lightNumber)+' .')
         lightAndShoot(target_folder,batchNumber,lightNumber)
 
 batchLightAndShoot(base_path,batchNumber)
